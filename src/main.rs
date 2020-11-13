194971662a8d7acc41b81628e68a4da789fd0216
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2020-11-13 17:13:26 -0500

    Discard all buffers that do not start with expected character.

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,19 @@ use rppal::uart::{Parity, Uart};
 use std::error::Error;
 use std::time::Duration;
 
+const DATA_FRAME_SIZE: usize = 7;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut uart = Uart::new(9600, Parity::None, 8, 1)?;
 
     uart.set_read_mode(1, Duration::default())?;
 
-    const DATA_FRAME_SIZE: usize = 7;
-
-    let mut first_val = [0u8; 1];
-    let mut buffer = [0u8; DATA_FRAME_SIZE - 1];
+    let mut buffer = [0u8; DATA_FRAME_SIZE];
     loop {
         // Fill the buffer variable with any incoming data.
-        if uart.read(&mut first_val)? > 0 {
-            if first_val[0] != 104 {
-                println!("skipped byte: {:?}", first_val);
-            } else {
-                // Fill the buffer variable with any incoming data.
-                if uart.read(&mut buffer)? > 0 {
-                    println!("Received bytes: {:?}{:?}", first_val, buffer);
-                }
+        if uart.read(&mut buffer)? > 0 {
+            if buffer[0] == 104 {
+                println!("Received bytes: {:?}", buffer);
             }
         }
     }
